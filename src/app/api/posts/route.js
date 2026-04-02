fa0c1ba9ff528d541166ab982ebdad7222683877
Trthao1902 <148039584+Trthao1902@users.noreply.github.com>
2026-04-02 22:27:22 +0700

    update[3]

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,10 +1,17 @@
 import prisma from "@/app/utils/connect";
 import { NextResponse } from "next/server"
 
-export const GET = async () => {
+export const GET = async (req) => {
+
+    const { searchParams } = new URL(req.url);
+
+    const page = searchParams.get("page")
+
+    const POSTS_PER_PAGE = 2;
+
     try {
 
-        const posts = await prisma.post.findMany()
+        const posts = await prisma.post.findMany({take: POSTS_PER_PAGE, skip: POSTS_PER_PAGE * (page - 1)});
 
         return new NextResponse(
             JSON.stringify(posts, { status: 200 })
