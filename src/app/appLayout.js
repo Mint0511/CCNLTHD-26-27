3b7e18d822629c6a9515ecdee75e736eafa31d23
nganvtt74 <nganvtt74@gmail.com>
2026-04-09 09:08:15 +0700

    Refactor AppLayout component to simplify structure and enhance separation of concerns

--- src/app/appLayout.js
+++ src/app/appLayout.js
@@ -1,30 +1,22 @@
-'use client';
+'use client'
 
-import { ThemeContextProvider } from '@/context/ThemeContext';
-import ThemeProvider from '@/providers/ThemeProvider';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import { ThemeContextProvider } from '@/context/ThemeContext'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import ThemeProvider from '@/providers/ThemeProvider'
 
-// 1. Tách biệt phần giao diện (UI Layout) thành một component riêng biệt
-const CoreLayout = ({ children }) => (
-  <div className="container">
-    <div className="wrapper">
-      <Navbar />
-      <main className="content">{children}</main>
-      <Footer />
-    </div>
-  </div>
-);
-
-// 2. AppLayout giờ đây chỉ đóng vai trò là "Provider Tree" (Phân tách trách nhiệm - Separation of Concerns)
 export default function AppLayout({ children }) {
   return (
     <ThemeContextProvider>
       <ThemeProvider>
-        <CoreLayout>
-          {children}
-        </CoreLayout>
+        <div className="container">
+          <div className='wrapper'>
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>  
       </ThemeProvider>
     </ThemeContextProvider>
-  );
+  )
 }
