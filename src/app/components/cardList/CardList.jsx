1647280b78051611dfdfdc441ec9a1b9adc87ad3
Van Minh <minhvn0511@gmail.com>
2026-04-09 13:33:42 +0700

    Update CardList.jsx

--- src/app/components/cardList/CardList.jsx
+++ src/app/components/cardList/CardList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './cardList.module.css'
-import Pagination from '../Pagination/Pagination'
-import Card from '../Card/Card'
+import Pagination from '../pagination/Pagination'
+import Card from '../card/Card'
 
 // Hàm fetch dữ liệu từ API: chấp nhận trang (page), danh mục (cat) và từ khóa tìm kiếm (search)
 const getData = async (page, cat, search) => {
