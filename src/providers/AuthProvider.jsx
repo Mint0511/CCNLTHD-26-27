11324e0615d524697c93119f6531bd8a4eab60f6
nganvtt74 <nganvtt74@gmail.com>
2026-04-09 09:01:26 +0700

    Refactor AuthProvider component for concise syntax

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,12 +2,8 @@
 
 import { SessionProvider } from "next-auth/react";
 
-const AuthProvider = ({ children }) => {
-  return (
-    <SessionProvider refetchInterval={0}>
-      {children}
-    </SessionProvider>
-  );
-};
+const AuthProvider = ({ children }) => (
+  <SessionProvider refetchInterval={0}>{children}</SessionProvider>
+);
 
 export default AuthProvider;
